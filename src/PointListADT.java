acf107bbabf79bf5f240aa8f81dd2d48560cdeca
dbc2201 <divyanshb22@gmail.com>
2018-12-09 22:26:04 +0530

    add member method 'goToBeginning()' (boolean)

--- src/PointListADT.java
+++ src/PointListADT.java
@@ -80,4 +80,18 @@ public class PointListADT
 		return false;
 	}
 	
+	/*
+	* Member Method for the Class. This method will move the cursor to the beginning of
+	* the list.
+	* */
+	boolean goToBeginning()
+	{
+		//	pre-condition	:	NONE.
+		//	post-condition	:	This method will check whether the list is empty or not,
+		//	if the list is not empty, then it will move the cursor to the point at the
+		//	beginning of the list and return TRUE, otherwise, this method will return
+		//	FALSE.
+		return false;
+	}
+	
 }
