852f3fce8c18da1966866d27284cc552c6f380e5
dbc2201 <divyanshb22@gmail.com>
2018-12-09 21:56:13 +0530

    add default constructor

--- src/PointListADT.java
+++ src/PointListADT.java
@@ -1,4 +1,13 @@
 public class PointListADT
 {
-
+	/*
+	* Constructor Method for the class.
+	* */
+	PointListADT()
+	{
+		//	pre-condition	:	none
+		//	post-condition	:	This default constructor will call the 'setup()'
+		//	method(which creates an empty list). It allocates enough memory for
+		//	a list containing 'DEF_	MAX_LIST_SIZE' points.(it is a constant value)
+	}
 }
